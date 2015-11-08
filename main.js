2668d8902a53107ba754dce4953a0f46c416f930
Sumin Byeon <suminb@gmail.com>
2015-11-09 00:31:19 +0900

    Getting started

---
+++ main.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function() {
+  var baseDate = new Date(Date.parse('2015-11-08 15:40:00 GMT+0900'));
+
+	tick(baseDate);
+	setInterval(function() {
+		tick(baseDate);
+	}, 1000);
+});
+
+function tick(baseDate) {
+	var currentTime = new Date();
+
+	var diff = currentTime.getTime() - baseDate.getTime();
+
+	$("#timer").text(format(diff / 1000));
+
+}
+
+function format(seconds) {
+	var days = 0;
+	var hours = 0;
+	var minutes = 0;
+
+	if (seconds >= 3600 * 24) {
+		days = parseInt(seconds / (3600 * 24));
+		seconds = seconds % (3600 * 24);
+	}
+	if (seconds >= 3600) {
+		hours = parseInt(seconds / 3600);
+		seconds = seconds % 3600;
+	}
+	if (seconds >= 60) {
+		minutes = parseInt(seconds / 60);
+		seconds = seconds % 60;
+	}
+	seconds = parseInt(seconds);
+
+	var fd = $.sprintf("%02d:%02d:%02d", hours, minutes, seconds);
+	if (days > 0) fd = $.sprintf("%d days %s", days, fd);
+
+	return fd;
+}
